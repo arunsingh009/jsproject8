34fee9a4ed7cfb9f65440917ed648908a6aa40e9
Arun Kumar Singh <66819980+arunsingh009@users.noreply.github.com>
2021-12-25 00:41:23 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var images = ['./images/pic1.jpg','./images/pic2.jpg','./images/pic3.jpg','./images/pic4.jpg','./images/pic5.jpg','./images/pic6.jpg'];
+var images = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg','pic6.jpg'];
 let imgIndex=0;
    
 function forwardImage(){
